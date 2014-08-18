aa8671c26291432bcf18df70028d6512b8465e86
Oliver King <Natron3030@gmail.com>
2014-08-18 15:07:23 -0700

    Support 0/1 boolean values when deserializing dart messages. This is already supported by other serialization libraries and will make it easier to reuse serialized messages from other frameworks.

--- lib/src/protobuf/generated_message.dart
+++ lib/src/protobuf/generated_message.dart
@@ -752,8 +752,15 @@ abstract class GeneratedMessage {
         } else if (value == 'false') {
           return false;
         }
-      }
       expectedType = 'bool, "true", or "false"';
+      } else if (value is num) {
+        if (value == 1) {
+          return true;
+        } else if (value == 0) {
+          return false;
+        }
+        expectedType = 'num, 0, or 1';
+      }
       break;
     case _BYTES_BIT:
       if (value is String) {
