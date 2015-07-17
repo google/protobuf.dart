0a75ca63c7b3709b208111f6c0689c2016be09bd
Brian Slesinsky <skybrian@google.com>
2015-07-16 17:30:26 -0700

    make pb_list_test a bit easier to read (ran dartformat and cleaned up)

--- test/pb_list_test.dart
+++ test/pb_list_test.dart
@@ -9,7 +9,7 @@ import 'package:protobuf/protobuf.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('testPbList', () {
+  test('testPbList handles basic operations', () {
     PbList<int> lb1 = new PbList();
     expect(lb1, []);
 
@@ -37,19 +37,25 @@ void main() {
     expect(lb1.last, 6);
 
     int count = 0;
-    lb1.forEach((int i) {count += i;});
+    lb1.forEach((int i) {
+      count += i;
+    });
     expect(count, 108);
 
-    List<int> evens = new List<int>.from(
-        lb1.where((int i) { return i % 2 == 0; }));
+    isEven(int i) => i % 2 == 0;
+    List<int> evens = new List<int>.from(lb1.where(isEven));
     expect(evens, [0, 2, 6]);
 
-    expect(lb1.any((int i) { return i % 2 == 0; }), isTrue);
-    expect(lb1.every((int i) { return i >= 0; }), isTrue);
+    expect(lb1.any(isEven), isTrue);
+
+    isNonNegative(int i) => i >= 0;
+    expect(lb1.every(isNonNegative), isTrue);
 
     lb1.clear();
     expect(lb1, []);
+  });
 
+  test('PbList handles range operations', () {
     PbList<int> lb2 = new PbList();
 
     lb2.addAll([1, 2, 3, 4, 5, 6, 7, 8, 9]);
@@ -75,49 +81,71 @@ void main() {
 
     lb2.setRange(5, 6, [88, 99].take(2), 1);
     expect(lb2, [1, 2, 3, 9, 8, 99]);
+  });
 
-    expect(() { (new PbList<int>() as dynamic).add('hello'); }, throws);
+  test('PbList validates items', () {
+    expect(() {
+      (new PbList<int>() as dynamic).add('hello');
+    }, throws);
+  });
 
+  test('Pbint32List validates items', () {
     PbSint32List listSint32 = new PbSint32List();
 
-    expect(() { listSint32.add(-2147483649); }, throwsArgumentError);
+    expect(() {
+      listSint32.add(-2147483649);
+    }, throwsArgumentError);
 
-    expect(() { listSint32.add(-2147483648); },
-        returnsNormally,
-        reason: 'could not add min_sint32 to PbSint32List');
+    expect(() {
+      listSint32.add(-2147483648);
+    }, returnsNormally, reason: 'could not add min_sint32 to PbSint32List');
 
-    expect(() { listSint32.add(2147483648); }, throwsArgumentError);
+    expect(() {
+      listSint32.add(2147483648);
+    }, throwsArgumentError);
 
-    expect(() { listSint32.add(2147483647); },
-        returnsNormally,
-        reason: 'could not add max_sint32 to PbSint32List');
+    expect(() {
+      listSint32.add(2147483647);
+    }, returnsNormally, reason: 'could not add max_sint32 to PbSint32List');
+  });
 
+  test('PBUint32List validates items', () {
     PbUint32List listUint32 = new PbUint32List();
 
-    expect(() { listUint32.add(-1); }, throwsArgumentError);
+    expect(() {
+      listUint32.add(-1);
+    }, throwsArgumentError);
 
-    expect(() { listUint32.add(0); },
-        returnsNormally,
-        reason: 'could not add 0 PbSint32List');
+    expect(() {
+      listUint32.add(0);
+    }, returnsNormally, reason: 'could not add 0 PbSint32List');
 
-    expect(() { listUint32.add(4294967296); }, throwsArgumentError);
+    expect(() {
+      listUint32.add(4294967296);
+    }, throwsArgumentError);
 
-    expect(() { listUint32.add(4294967295); },
-        returnsNormally,
-        reason: 'could not add max_uint32 to PbUint32List');
+    expect(() {
+      listUint32.add(4294967295);
+    }, returnsNormally, reason: 'could not add max_uint32 to PbUint32List');
+  });
 
+  test('PbFloatList validates items', () {
     PbFloatList listFloat = new PbFloatList();
 
-    expect(() { listFloat.add(3.4028234663852886E39); }, throwsArgumentError);
+    expect(() {
+      listFloat.add(3.4028234663852886E39);
+    }, throwsArgumentError);
 
-    expect(() { listFloat.add(-3.4028234663852886E39); }, throwsArgumentError);
+    expect(() {
+      listFloat.add(-3.4028234663852886E39);
+    }, throwsArgumentError);
 
-    expect(() { listFloat.add(3.4028234663852886E38); },
-        returnsNormally,
-        reason: 'could not add max_float to PbUint64List');
+    expect(() {
+      listFloat.add(3.4028234663852886E38);
+    }, returnsNormally, reason: 'could not add max_float to PbUint64List');
 
-    expect(() { listFloat.add(-3.4028234663852886E38); },
-        returnsNormally,
-        reason: 'could not add max_float to PbUint64List');
+    expect(() {
+      listFloat.add(-3.4028234663852886E38);
+    }, returnsNormally, reason: 'could not add max_float to PbUint64List');
   });
 }
