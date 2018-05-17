9bb7b9b8a2a0884a5577911d495210ce3117fa9e
Vyacheslav Egorov <vegorov@google.com>
2018-05-17 18:28:45 +0200

    Introduce serialization/deserialization benchmarks
    These are based on benchmarks from original google/protobuf library.

---
+++ benchmarks/benchmark_js.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+/// JS protobuf serialization/deserialization benchmark.
+///
+/// Loads benchmark [Dataset]s from the predefined list of files
+/// using D8's builtin readbuffer.
+library benchmark_js;
+
+import 'common.dart';
+import 'd8.dart';
+
+final files = [
+  "benchmarks/datasets/google_message1/proto3/dataset.google_message1_proto3.pb",
+  "benchmarks/datasets/google_message1/proto2/dataset.google_message1_proto2.pb",
+  "benchmarks/datasets/google_message2/dataset.google_message2.pb"
+];
+
+void main(List<String> arguments) {
+  final List<Dataset> datasets = files
+      .map((file) => new Dataset.fromBinary(readAsBytesSync(file)))
+      .toList(growable: false);
+
+  new FromBinaryBenchmark(datasets).report();
+  new ToBinaryBenchmark(datasets).report();
+  new ToJsonBenchmark(datasets).report();
+  new FromJsonBenchmark(datasets).report();
+}
---
+++ benchmarks/compile-protos.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+# Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+# for details. All rights reserved. Use of this source code is governed by a
+# BSD-style license that can be found in the LICENSE file.
+
+# Compile all protos involed in the benchmarking using protoc compiler.
+
+PROTOS=(
+  "benchmarks.proto"
+  "datasets/google_message1/proto2/benchmark_message1_proto2.proto"
+  "datasets/google_message1/proto3/benchmark_message1_proto3.proto"
+  "datasets/google_message2/benchmark_message2.proto"
+  "datasets/google_message3/benchmark_message3.proto"
+  "datasets/google_message3/benchmark_message3_1.proto"
+  "datasets/google_message3/benchmark_message3_2.proto"
+  "datasets/google_message3/benchmark_message3_3.proto"
+  "datasets/google_message3/benchmark_message3_4.proto"
+  "datasets/google_message3/benchmark_message3_5.proto"
+  "datasets/google_message3/benchmark_message3_6.proto"
+  "datasets/google_message3/benchmark_message3_7.proto"
+  "datasets/google_message3/benchmark_message3_8.proto"
+  "datasets/google_message4/benchmark_message4.proto"
+  "datasets/google_message4/benchmark_message4_1.proto"
+  "datasets/google_message4/benchmark_message4_2.proto"
+  "datasets/google_message4/benchmark_message4_3.proto"
+)
+
+mkdir -p temp
+
+for proto in ${PROTOS[@]}
+do
+  echo "PROTOC ${proto}"
+  protoc --dart_out=temp $proto
+done
