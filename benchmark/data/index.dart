feab39e0592398775ca2c3752f3ae5b5e00a2c0a
Kevin Moore <kevmoo@users.noreply.github.com>
2018-09-25 08:16:57 -0700

    Replace hand-maintained list of previous benchmarks with pkg:build (#128)
    MUCH easier to maintain

--- benchmark/data/index.dart
+++ benchmark/data/index.dart
@@ -11,27 +11,3 @@ const String pubspecYamlName = "pubspec.link.yaml";
 const String pubspecLockName = "pubspec.link.lock";
 
 const String hostfileName = "hostname.txt";
-
-List<String> allReportNames = const [
-  latestVMReportName,
-  "skybrian5/0.4.2/json_chrome.pb.json",
-  "skybrian5/0.4.2/json_vm.pb.json",
-  "skybrian5/0.4.2/props_chrome.pb.json",
-  "skybrian5/0.4.2/props_vm.pb.json",
-  "skybrian5/head/json_chrome.pb.json",
-  "skybrian5/head/json_vm.pb.json",
-  "skybrian5/head/props_chrome.pb.json",
-  "skybrian5/head/props_vm.pb.json",
-  "skybrian-macbookpro/0.4.2/json_chrome.pb.json",
-  "skybrian-macbookpro/0.4.2/json_vm.pb.json",
-  "skybrian-macbookpro/0.4.2/props_chrome.pb.json",
-  "skybrian-macbookpro/0.4.2/props_vm.pb.json",
-  "skybrian-macbookpro/0.5.0/json_chrome.pb.json",
-  "skybrian-macbookpro/0.5.0/json_vm.pb.json",
-  "skybrian-macbookpro/0.5.0/props_chrome.pb.json",
-  "skybrian-macbookpro/0.5.0/props_vm.pb.json",
-  "skybrian-macbookpro/head/json_chrome.pb.json",
-  "skybrian-macbookpro/head/json_vm.pb.json",
-  "skybrian-macbookpro/head/props_chrome.pb.json",
-  "skybrian-macbookpro/head/props_vm.pb.json",
-];
