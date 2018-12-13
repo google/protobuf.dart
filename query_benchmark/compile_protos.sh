8d8eb6e5749041703e3b1bad5fc40fc3b3a3b159
Sigurd Meldgaard <sigurdm@google.com>
2018-12-13 15:44:32 +0100

    Use explicit protoc_plugin

--- query_benchmark/compile_protos.sh
+++ query_benchmark/compile_protos.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 rm -rf lib/generated
 mkdir lib/generated
-protoc --dart_out=lib/generated -Iprotos protos/*.proto
+protoc --dart_out=lib/generated -Iprotos protos/*.proto --plugin=../protoc_plugin/protoc_gen_dart
 dartfmt -w lib/generated
