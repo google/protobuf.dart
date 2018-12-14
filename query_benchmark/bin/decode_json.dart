d41d9cb90d6d39a0a7d66d5b5ba042cde75bb2c9
Sigurd Meldgaard <sigurdm@google.com>
2018-12-14 13:39:18 +0100

    Adding benchmarks for running in golem (#142)
    * first stab at a benchmark for running in golem.

---
+++ query_benchmark/bin/decode_json.dart
@@ -0,0 +1,21 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:query_benchmark/generated/f0.pb.dart' as f0;
+import 'package:query_benchmark/benchmark.dart';
+import 'package:query_benchmark/readfile.dart';
+
+main() {
+  String path = const String.fromEnvironment('testfile') ?? 'testdata/500.pb';
+
+  List<int> encoded = readfile(path);
+  f0.A0 a = f0.A0.fromBuffer(encoded);
+  String json = a.writeToJson();
+  print(
+    formatReport(
+      title: 'protobuf_decode_json',
+      duration: measure(() => f0.A0.fromJson(json)),
+    ),
+  );
+}
---
+++ query_benchmark/bin/encode_json.dart
@@ -0,0 +1,20 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:query_benchmark/generated/f0.pb.dart' as f0;
+import 'package:query_benchmark/benchmark.dart';
+import 'package:query_benchmark/readfile.dart';
+
+main() {
+  String path = const String.fromEnvironment('testfile') ?? 'testdata/500.pb';
+
+  List<int> encoded = readfile(path);
+  f0.A0 a = f0.A0.fromBuffer(encoded);
+  print(
+    formatReport(
+      title: 'protobuf_encode_json',
+      duration: measure(() => a.writeToJson()),
+    ),
+  );
+}
