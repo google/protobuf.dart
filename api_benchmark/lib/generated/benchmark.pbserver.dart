65b06d0f5b85b29ec3c0a1019374e357ba7d91a9
Sigurd Meldgaard <sigurdm@google.com>
2019-01-10 13:39:54 +0100

    Remove generated files

--- api_benchmark/lib/generated/benchmark.pbserver.dart
+++
@@ -1,8 +0,0 @@
-///
-//  Generated code. Do not modify.
-//  source: benchmark.proto
-///
-// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
-
-export 'benchmark.pb.dart';
-
--- api_benchmark/lib/generated/string_grid.pbserver.dart
+++
@@ -1,8 +0,0 @@
-///
-//  Generated code. Do not modify.
-//  source: string_grid.proto
-///
-// ignore_for_file: non_constant_identifier_names,library_prefixes,unused_import
-
-export 'string_grid.pb.dart';
-
