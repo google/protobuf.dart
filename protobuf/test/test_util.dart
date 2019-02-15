e43ea4d91de47c77eaae82bde634d24f3b84927c
Nic Hite <nichite@google.com>
2019-02-15 01:46:18 -0800

    Add more words to the Dart reserved names list. (#202)
    * Add more words to the Dart reserved names list.
    
    * Fix explicit default to null values throughout protobuf code.

--- protobuf/test/test_util.dart
+++ protobuf/test/test_util.dart
@@ -7,12 +7,12 @@ library test_util;
 import 'package:fixnum/fixnum.dart';
 import 'package:test/test.dart';
 
-Int64 make64(lo, [hi = null]) {
+Int64 make64(int lo, [int hi]) {
   if (hi == null) hi = lo < 0 ? -1 : 0;
   return new Int64.fromInts(hi, lo);
 }
 
-expect64(lo, [hi = null]) {
+expect64(int lo, [int hi]) {
   final Int64 expected = make64(lo, hi);
   return predicate((Int64 actual) => actual == expected);
 }
