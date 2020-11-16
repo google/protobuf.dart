98cf95889b5900b8a1b707f339398eadf056649f
Pieter van Loon <git@pietervanloon.com>
2020-11-17 00:58:52 +0100

    Add required annotation

--- protoc_plugin/lib/message_generator.dart
+++ protoc_plugin/lib/message_generator.dart
@@ -366,7 +366,7 @@ class MessageGenerator extends ProtobufContainer {
                 '  ${field.baseType.getRepeatedDartTypeIterable(fileGen)}? ${field.memberNames.fieldName},');
           } else {
             out.println(
-                '  ${field.getDartType(fileGen)}${field.isRequired ? "" : "?"} ${field.memberNames.fieldName},');
+                '  ${field.isRequired ? "required " : ""}${field.getDartType(fileGen)}${field.isRequired ? "" : "?"} ${field.memberNames.fieldName},');
           }
         }
         out.print('}');
