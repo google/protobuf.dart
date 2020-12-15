75389dd16f46ca2a4694f4d015240514c7e34077
Martin Kustermann <kustermann@google.com>
2020-12-15 17:00:05 +0100

    Allow dart protos to be sent through sendports (and therefore to other isolates) (#452)
    Decoded protos in Dart are represented in memory as instances of
    `GeneratedMessage` subclasses. The `GeneratedMessage` stores the field
    state in a `_FieldSet` and `UnknownFieldSet`.
    
    The `_FieldSet._meta` field points to a `BuilderInfo` object, which
    represents the metadata of the proto message.
    
    Sending this reference to `BuilderInfo` makes it hard to send dart
    protos to other isolates, because
    
      a) `BuilderInfo` hangs on to non-static closures, which cannot
          be sent to to other isolates atm
    
      b) Sending a transitive copy would copy the `BuilderInfo` object (and
         it's transitive closure). That can cause big waste of memory
         since an isolate would have this metadata possibly many times.
    
    To avoid these two issues, this CL does the following:
    
      * We replace `_FieldSet._meta` with a getter that accesses the
        `BuilderInfo` via the `_FieldSet._message.info_` getter. This getter
        is implemented in `GeneratedMessage` subclasses and reads from a
        static field.
    
        => We effectively replace a load of the `_meta` field with load +
           interface call + static field load.
    
      * To avoid performance regressions we avoid accessing the new
        `_FieldSet._meta` getter in the hot decoding loops. Instead we
        obtain this metadata once and pass it down to various methods that
        are part of decoding.
    
      * For similar reasons, we remove the `PbMap._entryBuilderInfo`
        field - which also refers to a `BuilderInfo`. Instead we obtain the
        neccessary map-specific `BuilderInfo` in the main decoder loop.
    
    This CL has to remove the 2 year long deprecated `PbMap.add()` functiondeprecated.

--- protobuf/lib/src/protobuf/field_info.dart
+++ protobuf/lib/src/protobuf/field_info.dart
@@ -176,8 +176,8 @@ class FieldInfo<T> {
 
   /// Convenience method to thread this FieldInfo's reified type parameter to
   /// _FieldSet._ensureRepeatedField.
-  List<T?> _ensureRepeatedField(_FieldSet fs) {
-    return fs._ensureRepeatedField<T>(this);
+  List<T?> _ensureRepeatedField(BuilderInfo meta, _FieldSet fs) {
+    return fs._ensureRepeatedField<T>(meta, this);
   }
 
   @override
@@ -192,6 +192,9 @@ String _unCamelCase(String name) {
 }
 
 class MapFieldInfo<K, V> extends FieldInfo<PbMap<K, V>?> {
+  static dynamic throwYouShouldNotCallThis() =>
+      throw StateError('You should not call this.');
+
   final int? keyFieldType;
   final int? valueFieldType;
 
@@ -213,8 +216,7 @@ class MapFieldInfo<K, V> extends FieldInfo<PbMap<K, V>?> {
       this.valueCreator,
       {String? protoName})
       : super(name, tagNumber, index, type,
-            defaultOrMaker: () =>
-                PbMap<K, V>(keyFieldType, valueFieldType, mapEntryBuilderInfo),
+            defaultOrMaker: () => throwYouShouldNotCallThis,
             protoName: protoName) {
     ArgumentError.checkNotNull(name, 'name');
     ArgumentError.checkNotNull(tagNumber, 'tagNumber');
@@ -225,8 +227,8 @@ class MapFieldInfo<K, V> extends FieldInfo<PbMap<K, V>?> {
   FieldInfo get valueFieldInfo =>
       mapEntryBuilderInfo.fieldInfo[PbMap._valueFieldNumber]!;
 
-  Map<K, V> _ensureMapField(_FieldSet fs) {
-    return fs._ensureMapField<K, V>(this);
+  Map<K, V> _ensureMapField(BuilderInfo meta, _FieldSet fs) {
+    return fs._ensureMapField<K, V>(meta, this);
   }
 
   Map<K, V> _createMapField(GeneratedMessage m) {
