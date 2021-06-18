3cc5e7bd5ba27cbc34de95e5614c8ed3873ec67e
Michael Thomsen <mit@google.com>
2021-06-18 09:53:22 +0200

    Switch to `dart format`

--- api_benchmark/compile_protos.sh
+++ api_benchmark/compile_protos.sh
@@ -2,4 +2,4 @@
 rm -rf lib/generated
 mkdir lib/generated
 protoc --dart_out=lib/generated --plugin=protoc-gen-dart=run_protoc_plugin.sh -Iprotos protos/*.proto
-dartfmt -w lib/generated
+dart format lib/generated
