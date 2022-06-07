a73718504e9bb3217d4f07297da962790d028425
Ömer Sinan Ağacan <omersa@google.com>
2022-06-07 16:23:13 +0200

    Rename jsonWriter -> jsonSink

--- protobuf/lib/src/protobuf/proto3_json_writer.dart
+++ protobuf/lib/src/protobuf/proto3_json_writer.dart
@@ -5,13 +5,13 @@
 part of protobuf;
 
 void _writeToProto3JsonSink(
-    _FieldSet fs, TypeRegistry typeRegistry, JsonSink jsonWriter) {
+    _FieldSet fs, TypeRegistry typeRegistry, JsonSink jsonSink) {
   final wellKnownConverter = fs._meta.toProto3Json;
   if (wellKnownConverter != null) {
     throw 'Well-known types not supported yet';
   }
 
-  jsonWriter.startObject(); // start message
+  jsonSink.startObject(); // start message
 
   for (FieldInfo fieldInfo in fs._infosSortedByTag) {
     var value = fs._values[fieldInfo.index!];
@@ -20,47 +20,47 @@ void _writeToProto3JsonSink(
       continue; // It's missing, repeated, or an empty byte array.
     }
 
-    jsonWriter.addKey(fieldInfo.name);
+    jsonSink.addKey(fieldInfo.name);
 
     if (fieldInfo.isMapField) {
-      jsonWriter.startObject(); // start map field
+      jsonSink.startObject(); // start map field
       final MapFieldInfo mapEntryInfo = fieldInfo as MapFieldInfo;
       for (MapEntry entry in (value as PbMap).entries) {
         final key = entry.key;
         final value = entry.value;
-        _writeMapKey(key, mapEntryInfo.keyFieldType, jsonWriter);
+        _writeMapKey(key, mapEntryInfo.keyFieldType, jsonSink);
         _writeFieldValue(
-            value, mapEntryInfo.valueFieldType, jsonWriter, typeRegistry);
+            value, mapEntryInfo.valueFieldType, jsonSink, typeRegistry);
       }
-      jsonWriter.endObject(); // end map field
+      jsonSink.endObject(); // end map field
     } else if (fieldInfo.isRepeated) {
-      jsonWriter.startArray(); // start repeated field
+      jsonSink.startArray(); // start repeated field
       for (final element in value as PbListBase) {
-        _writeFieldValue(element, fieldInfo.type, jsonWriter, typeRegistry);
+        _writeFieldValue(element, fieldInfo.type, jsonSink, typeRegistry);
       }
-      jsonWriter.endArray(); // end repeated field
+      jsonSink.endArray(); // end repeated field
     } else {
-      _writeFieldValue(value, fieldInfo.type, jsonWriter, typeRegistry);
+      _writeFieldValue(value, fieldInfo.type, jsonSink, typeRegistry);
     }
   }
 
-  jsonWriter.endObject(); // end message
+  jsonSink.endObject(); // end message
 }
 
-void _writeMapKey(dynamic key, int keyType, JsonSink jsonWriter) {
+void _writeMapKey(dynamic key, int keyType, JsonSink jsonSink) {
   var baseType = PbFieldType._baseType(keyType);
 
   assert(!_isRepeated(keyType));
 
   switch (baseType) {
     case PbFieldType._BOOL_BIT:
-      jsonWriter.addKey((key as bool).toString());
+      jsonSink.addKey((key as bool).toString());
       break;
     case PbFieldType._STRING_BIT:
-      jsonWriter.addKey(key as String);
+      jsonSink.addKey(key as String);
       break;
     case PbFieldType._UINT64_BIT:
-      jsonWriter.addKey((key as Int64).toStringUnsigned().toString());
+      jsonSink.addKey((key as Int64).toStringUnsigned().toString());
       break;
     case PbFieldType._INT32_BIT:
     case PbFieldType._SINT32_BIT:
@@ -71,63 +71,63 @@ void _writeMapKey(dynamic key, int keyType, JsonSink jsonWriter) {
     case PbFieldType._SINT64_BIT:
     case PbFieldType._SFIXED64_BIT:
     case PbFieldType._FIXED64_BIT:
-      jsonWriter.addKey(key.toString());
+      jsonSink.addKey(key.toString());
       break;
     default:
       throw StateError('Not a valid key type $keyType');
   }
 }
 
-void _writeFieldValue(dynamic fieldValue, int fieldType, JsonSink jsonWriter,
+void _writeFieldValue(dynamic fieldValue, int fieldType, JsonSink jsonSink,
     TypeRegistry typeRegistry) {
   if (fieldValue == null) {
-    jsonWriter.addNull();
+    jsonSink.addNull();
     return;
   }
 
   if (_isGroupOrMessage(fieldType)) {
     _writeToProto3JsonSink(
-        (fieldValue as GeneratedMessage)._fieldSet, typeRegistry, jsonWriter);
+        (fieldValue as GeneratedMessage)._fieldSet, typeRegistry, jsonSink);
   } else if (_isEnum(fieldType)) {
-    jsonWriter.addString((fieldValue as ProtobufEnum).name);
+    jsonSink.addString((fieldValue as ProtobufEnum).name);
   } else {
     final baseType = PbFieldType._baseType(fieldType);
     switch (baseType) {
       case PbFieldType._BOOL_BIT:
-        jsonWriter.addBool(fieldValue);
+        jsonSink.addBool(fieldValue);
         break;
       case PbFieldType._STRING_BIT:
-        jsonWriter.addString(fieldValue);
+        jsonSink.addString(fieldValue);
         break;
       case PbFieldType._INT32_BIT:
       case PbFieldType._SINT32_BIT:
       case PbFieldType._UINT32_BIT:
       case PbFieldType._FIXED32_BIT:
       case PbFieldType._SFIXED32_BIT:
-        jsonWriter.addNumber(fieldValue);
+        jsonSink.addNumber(fieldValue);
         break;
       case PbFieldType._INT64_BIT:
       case PbFieldType._SINT64_BIT:
       case PbFieldType._SFIXED64_BIT:
       case PbFieldType._FIXED64_BIT:
-        jsonWriter.addString(fieldValue.toString());
+        jsonSink.addString(fieldValue.toString());
         break;
       case PbFieldType._FLOAT_BIT:
       case PbFieldType._DOUBLE_BIT:
         double value = fieldValue;
         if (value.isNaN) {
-          jsonWriter.addString(nan);
+          jsonSink.addString(nan);
         } else if (value.isInfinite) {
-          jsonWriter.addString(value.isNegative ? negativeInfinity : infinity);
+          jsonSink.addString(value.isNegative ? negativeInfinity : infinity);
         } else {
-          jsonWriter.addNumber(value);
+          jsonSink.addNumber(value);
         }
         break;
       case PbFieldType._UINT64_BIT:
-        jsonWriter.addString((fieldValue as Int64).toStringUnsigned());
+        jsonSink.addString((fieldValue as Int64).toStringUnsigned());
         break;
       case PbFieldType._BYTES_BIT:
-        jsonWriter.addString(base64Encode(fieldValue));
+        jsonSink.addString(base64Encode(fieldValue));
         break;
       default:
         throw StateError(
