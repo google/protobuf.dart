54d83953d4b82088eb9b31c65800266f30efad7e
Ömer Sinan Ağacan <omersa@google.com>
2022-06-24 15:10:57 +0200

    Revert accidental change

--- query_benchmark/compile_protos.sh
+++ query_benchmark/compile_protos.sh
@@ -1,4 +1,4 @@
-#/usr/bin/env bash
+#!/usr/bin/env bash
 rm -rf lib/generated
 mkdir lib/generated
 protoc --dart_out=lib/generated --plugin=protoc-gen-dart=run_protoc_plugin.sh -Iprotos protos/*.proto
