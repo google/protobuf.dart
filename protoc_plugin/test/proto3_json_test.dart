3cfed309009a493eca9a8f6ec9a29ef2b6c10ee5
Ömer Sinan Ağacan <omersa@google.com>
2022-06-27 16:11:51 +0200

    Fix lint

--- protoc_plugin/test/proto3_json_test.dart
+++ protoc_plugin/test/proto3_json_test.dart
@@ -2,8 +2,8 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:core' hide Duration;
 import 'dart:convert' show jsonEncode;
+import 'dart:core' hide Duration;
 
 import 'package:fixnum/fixnum.dart';
 import 'package:protobuf/protobuf.dart';
