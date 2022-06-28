00a46ea4aafea8e022c6210d7d26be08c31d1d7a
Ömer Sinan Ağacan <omersa@google.com>
2022-06-28 10:41:10 +0200

    Fix compile_protos.sh

--- benchmarks/compile_protos.sh
+++ benchmarks/compile_protos.sh
@@ -4,6 +4,8 @@
 # for details. All rights reserved. Use of this source code is governed by a
 # BSD-style license that can be found in the LICENSE file.
 
+SCRIPT_DIR=$(dirname "${BASH_SOURCE}")
+
 # These protos don't have any imports
 SIMPLE_PROTOS=(
     "protos/google_message1_proto2.proto"
@@ -16,4 +18,10 @@ set -e
 
 mkdir -p lib/generated
 
-protoc --dart_out=lib/generated --plugin=protoc-gen-dart=run_protoc_plugin.sh "${PROTOS[@]/#/$SCRIPT_DIR/}"
+protoc --dart_out=lib/generated --plugin=protoc-gen-dart=run_protoc_plugin.sh \
+    -I$SCRIPT_DIR/protos \
+    "${SIMPLE_PROTOS[@]/#/$SCRIPT_DIR/}"
+
+protoc --dart_out=lib/generated --plugin=protoc-gen-dart=run_protoc_plugin.sh \
+    -I$SCRIPT_DIR/protos/query_benchmark \
+    $SCRIPT_DIR/protos/query_benchmark/*.proto
