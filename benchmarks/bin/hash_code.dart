b4e0a77fcde81d36cf46a47e72f0cc68a5c92190
Ömer Sinan Ağacan <omersa@google.com>
2022-06-28 11:39:57 +0200

    Add hash code bench

---
+++ benchmarks/bin/hash_code.dart
@@ -0,0 +1,46 @@
+// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
+import 'package:protobuf_benchmarks/readfile.dart';
+import 'package:protobuf_benchmarks/generated/google_message1_proto2.pb.dart'
+    as p2;
+import 'package:protobuf_benchmarks/generated/google_message1_proto3.pb.dart'
+    as p3;
+import 'package:protobuf_benchmarks/generated/google_message2.pb.dart';
+
+import 'dart:convert' show jsonEncode;
+
+import 'package:benchmark_harness/benchmark_harness.dart';
+
+class Benchmark extends BenchmarkBase {
+  late final p2.GoogleMessage1 _message1Proto2;
+  late final p3.GoogleMessage1 _message1Proto3;
+  late final GoogleMessage2 _message2;
+
+  Benchmark(String name, List<int> message1Proto2Input,
+      List<int> message1Proto3Input, List<int> message2Input)
+      : super(name) {
+    _message1Proto2 = p2.GoogleMessage1.fromBuffer(message1Proto2Input);
+    _message1Proto3 = p3.GoogleMessage1.fromBuffer(message1Proto3Input);
+    _message2 = GoogleMessage2.fromBuffer(message2Input);
+  }
+
+  @override
+  void run() {
+    _message1Proto2.hashCode;
+    _message1Proto3.hashCode;
+    _message2.hashCode;
+  }
+}
+
+void main() {
+  List<int> message1Proto2Input =
+      readfile('datasets/google_message1_proto2.pb');
+  List<int> message1Proto3Input =
+      readfile('datasets/google_message1_proto3.pb');
+  List<int> message2Input = readfile('datasets/google_message2.pb');
+  Benchmark('protobuf_hashCode', message1Proto2Input, message1Proto3Input,
+          message2Input)
+      .report();
+}
