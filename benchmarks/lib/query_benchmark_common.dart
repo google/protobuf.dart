ee2320af371622740e6d328198f01d8c239edef1
Ömer Sinan Ağacan <omersa@google.com>
2022-06-29 09:25:03 +0200

    Update `QueryBenchmark` documentation

--- benchmarks/lib/query_benchmark_common.dart
+++ benchmarks/lib/query_benchmark_common.dart
@@ -1,11 +1,18 @@
-// Copyright (c) 2018, the Dart project authors.  Please see the AUTHORS file
+// Copyright (c) 2022, the Dart project authors.  Please see the AUTHORS file
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
 import 'package:benchmark_harness/benchmark_harness.dart';
 
-/// `benchmark_harness-2` reports 10x the time the benchmarked code takes. This
-/// overrides the `measure` method to divide the result by 10.
+/// Use this class in the old "query_benchmark" benchmarks.
+///
+/// `benchmark_harness-2` reports 10x the time the benchmarked code takes
+/// (https://github.com/dart-lang/benchmark_harness/issues/30). Because
+/// query_benchmark previously did not use `benchmark_harness`, when we start
+/// using `benchmark_harness` we get 10x larger benchmark results. To avoid
+/// this, this class overrides the relevant [BenchmarkBase] method(s) and
+/// divides the result by 10 before reporting, making query_benchmark numbers
+/// same as before.
 abstract class QueryBenchmark extends BenchmarkBase {
   QueryBenchmark(String name) : super(name);
 
