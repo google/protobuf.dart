b79707a747a7fa6ea9e36ffb83771e7aedf75754
Ömer Sinan Ağacan <omersa@google.com>
2022-06-30 13:44:40 +0200

    Add/update copyright lines in protobuf/test (#699)
    list_equality_test.dart was added to g3 with the copyright lines, copy
    it over to open source.
    
    I also checked copyright lines in other files, added as necessary. Minor
    formatting fix to make them follow the same format.

--- protobuf/test/json_test.dart
+++ protobuf/test/json_test.dart
@@ -1,3 +1,7 @@
+// Copyright (c) 2013, the Dart project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+
 // Basic smoke tests for the GeneratedMessage JSON API.
 //
 // There are more JSON tests in the dart-protoc-plugin package.
