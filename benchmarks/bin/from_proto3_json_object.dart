de6a05fec54463b32128322d8a0bf31117e684dc
Ömer Sinan Ağacan <omersa@google.com>
2022-07-04 15:33:46 +0200

    Fix warnings

--- benchmarks/bin/from_proto3_json_object.dart
+++ benchmarks/bin/from_proto3_json_object.dart
@@ -2,8 +2,6 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
-import 'dart:convert' show jsonDecode;
-
 import 'package:protobuf_benchmarks/benchmark_base.dart';
 import 'package:protobuf_benchmarks/generated/google_message1_proto2.pb.dart'
     as p2;
