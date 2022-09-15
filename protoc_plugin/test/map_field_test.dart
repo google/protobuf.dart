google/protobuf.dart

commit 3ea789fe97a8e569aa5001631d09436a6ab8e685

Avoid redundant shallow copy when getting read-only map fields (#741)
`_FieldSet._$getMap` unnecessarily shallow-copies the map field when the
message is read-only.

If a message is read-only then all of its fields are read-only, so the
map fields are read-only. We don't need to copy them.

Copying map fields on every access can be expensive depending on the map
size, so this will potentially be a significant performance win.

(Also, if we had to copy, we would have to deep-copy, rather than
shallow-copy, so the code would be buggy if we needed to copy the field)

Also fix `_isReadonly` field initialization in `PbMap.unmodifiable`.

We do not use `PbMap.unmodifiable` and we don't have any users of it, but I
think that method will be useful when fixing #705, so I'm not removing it for
now.

protobuf/lib/src/protobuf/field_set.dart
@@ -410,8 +410,7 @@ class _FieldSet {
     assert(fi.isMapField);
 
     if (_isReadOnly) {
-      return PbMap<K, V>.unmodifiable(
-          PbMap<K, V>(fi.keyFieldType, fi.valueFieldType));
+      return PbMap<K, V>(fi.keyFieldType, fi.valueFieldType);
     }
 
     var map = fi._createMapField(_message!);

protobuf/lib/src/protobuf/pb_map.dart
@@ -31,7 +31,7 @@ class PbMap<K, V> extends MapBase<K, V> {
       : keyFieldType = other.keyFieldType,
         valueFieldType = other.valueFieldType,
         _wrappedMap = Map.unmodifiable(other._wrappedMap),
-        _isReadonly = other._isReadonly;
+        _isReadonly = true;
 
   @override
   V? operator [](Object? key) => _wrappedMap[key];

commit 1762b82fbf18100afd98da7b6e906d10c5cb14dc

Fix $_getMap return value when mutability when message is read-only
This was broken in #741

protobuf/lib/src/protobuf/field_set.dart
@@ -407,7 +407,8 @@ class _FieldSet {
     assert(fi.isMapField);
 
     if (_isReadOnly) {
-      return PbMap<K, V>(fi.keyFieldType, fi.valueFieldType);
+      return PbMap<K, V>.unmodifiable(
+          PbMap<K, V>(fi.keyFieldType, fi.valueFieldType));
     }
 
     var map = fi._createMapField(_message!);

protoc_plugin/test/map_field_test.dart
@@ -447,4 +447,11 @@ void main() {
       expect(message, TestMap()..int32ToEnumField[0] = TestMap_EnumValue.BAR);
     }
   });
+
+  test('Read-only message uninitialized map field value is read-only', () {
+    final msg = TestMap()..freeze();
+    expect(() {
+      msg.int32ToInt32Field[0] = 1;
+    }, throwsA(const TypeMatcher<UnsupportedError>()));
+  });
 }