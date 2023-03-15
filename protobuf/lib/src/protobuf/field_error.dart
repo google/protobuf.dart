65357e6d46f79b6366a6752e1c873aa1634cbb99
Kevin Moore <kevmoo@google.com>
2023-03-15 13:25:32 -0700

    Fix formatting with latest SDK

--- protobuf/lib/src/protobuf/field_error.dart
+++ protobuf/lib/src/protobuf/field_error.dart
@@ -81,7 +81,6 @@ CheckFunc getCheckFunction(int fieldType) {
     case PbFieldType._ENUM_BIT:
     case PbFieldType._GROUP_BIT:
     case PbFieldType._MESSAGE_BIT:
-
     case PbFieldType._INT64_BIT:
     case PbFieldType._SINT64_BIT:
     case PbFieldType._SFIXED64_BIT:
