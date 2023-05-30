c2457cc7c758957ccae3a716fcf97702b19c57eb
Kevin Moore <kevmoo@google.com>
2023-05-30 09:16:21 -0700

    regenerate

--- protoc_plugin/lib/src/generated/descriptor.pbenum.dart
+++ protoc_plugin/lib/src/generated/descriptor.pbenum.dart
@@ -7,7 +7,7 @@
 // ignore_for_file: annotate_overrides, camel_case_types
 // ignore_for_file: constant_identifier_names, library_prefixes
 // ignore_for_file: non_constant_identifier_names, prefer_final_fields
-// ignore_for_file: return_of_invalid_type, unnecessary_this
+// ignore_for_file: unnecessary_import, unnecessary_this, unused_import
 
 import 'dart:core' as $core;
 
