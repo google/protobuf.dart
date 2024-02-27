4479ad3e3076cdf625e26f9b63db78d1ba2a27cf
Ömer Sinan Ağacan <omersa@google.com>
2024-02-27 09:52:01 +0100

    Remove merged_stages again, update protobuf/mono_pkg.yaml

--- tool/ci.sh
+++ tool/ci.sh
@@ -68,14 +68,6 @@ for PKG in ${PKGS}; do
         dart analyze --fatal-infos || EXIT_CODE=$?
         ;;
       analyze_1)
-        echo 'dart analyze lib'
-        dart analyze lib || EXIT_CODE=$?
-        ;;
-      analyze_2)
-        echo 'dart analyze test'
-        dart analyze test || EXIT_CODE=$?
-        ;;
-      analyze_3)
         echo 'dart analyze --fatal-infos bin lib test'
         dart analyze --fatal-infos bin lib test || EXIT_CODE=$?
         ;;
