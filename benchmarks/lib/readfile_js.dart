2ffdb4aa6d2c356e30aabc27c295a07b23207e8d
Ömer Sinan Ağacan <omersa@google.com>
2025-02-05 11:33:16 +0100

    Fix Wasm benchmarks
    - Merge dart2js and dart2wasm `readfile` implementations, use
      `js_interop` instead of `package:js`, which dart2wasm no longer
      supports.
    
    - Update the command used to run dart2wasm.
    
    - Remove the separate benchmark target for dart2wasm's `--omit-checks`
      flag. The flag is now implied by `-O4`.

--- benchmarks/lib/readfile_js.dart
+++ benchmarks/lib/readfile_js.dart
@@ -2,17 +2,18 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+import 'dart:js_interop';
 import 'dart:typed_data';
-
-import 'package:js/js.dart';
 
 /// Read the file at the given [path].
 ///
 /// This relies on the `readbuffer` function provided by d8.
 @JS()
-external ByteBuffer readbuffer(String path);
+external JSArrayBuffer readbuffer(String path);
 
 /// Read the file at the given [path].
 Uint8List readfile(String path) {
-  return Uint8List.view(readbuffer(path));
+  // Copy the contents to a new `Uint8List` to make sure in dart2wasm we
+  // benchmark with the native arrays instead of JS arrays.
+  return Uint8List.fromList(readbuffer(path).toDart.asUint8List());
 }
