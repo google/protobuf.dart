6d0a1055f1d7b510ac3464eff2e197328ab20f72
Devon Carew <devoncarew@gmail.com>
2025-02-08 13:54:00 -0800

    refactor repo CI and analysis options files

--- tool/setup.sh
+++ tool/setup.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-wget -O protoc.zip https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protoc-3.17.3-linux-x86_64.zip
+wget -O protoc.zip \
+  https://github.com/protocolbuffers/protobuf/releases/download/v3.17.3/protoc-3.17.3-linux-x86_64.zip
 unzip -d protoc protoc.zip
+
 if [[ -z "${GITHUB_ENV}" ]]; then
-  # Local mono_repo presubmit run
+  # Local run
   export PATH=$PWD/protoc/bin:$PATH
 else
   # GitHub Actions
