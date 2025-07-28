f7f65d44a78bde0d1f2ae4040bdd79d6429adb3f
Ömer Sinan Ağacan <omersa@google.com>
2025-07-28 18:19:30 +0200

    CI: Also run plugin tests on Chrome, with dart2js and dart2wasm (#1031)

--- protoc_plugin/test/file_generator_test.dart
+++ protoc_plugin/test/file_generator_test.dart
@@ -2,6 +2,9 @@
 // for details. All rights reserved. Use of this source code is governed by a
 // BSD-style license that can be found in the LICENSE file.
 
+@TestOn('vm')
+library;
+
 import 'package:protoc_plugin/indenting_writer.dart';
 import 'package:protoc_plugin/protoc.dart';
 import 'package:protoc_plugin/src/gen/google/api/client.pb.dart';
