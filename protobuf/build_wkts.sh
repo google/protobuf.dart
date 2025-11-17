e0047ec81b5e1a1c739acec6e8ba8f8165a3321b
Ömer Ağacan <omersa@google.com>
2025-11-17 11:19:56 +0000

    Update script to remove empty enums

--- protobuf/build_wkts.sh
+++ protobuf/build_wkts.sh
@@ -12,3 +12,13 @@ protoc \
     --dart_out=lib/well_known_types \
     --plugin=protoc-gen-dart=$(realpath '../protoc_plugin/bin/protoc-gen-dart-debug') \
     google/protobuf/*.proto
+
+# Remove empty .pbenum files.
+rm lib/well_known_types/google/protobuf/any.pbenum.dart
+rm lib/well_known_types/google/protobuf/api.pbenum.dart
+rm lib/well_known_types/google/protobuf/duration.pbenum.dart
+rm lib/well_known_types/google/protobuf/empty.pbenum.dart
+rm lib/well_known_types/google/protobuf/field_mask.pbenum.dart
+rm lib/well_known_types/google/protobuf/source_context.pbenum.dart
+rm lib/well_known_types/google/protobuf/timestamp.pbenum.dart
+rm lib/well_known_types/google/protobuf/wrappers.pbenum.dart
